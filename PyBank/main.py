9692b679f2d54b58e9e71d43a0ee7df6fc5dfe92
Thomas Meacham <tmeach92@gmail.com>
2020-05-30 00:10:31 -0400

    updated py bank file

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+import os
+import csv
+
+#path to file we are using
+Pybankcsv = os.path.join("Resources/PyBank_data.csv")
+
+#assign variables
+total_months = 0
+net_profit_loss = 0
+previous_month_profit_loss = 0
+current_month_profit_loss = 0
+change_profit_loss = 0
+months = []
+change_profit_loss = []
+
